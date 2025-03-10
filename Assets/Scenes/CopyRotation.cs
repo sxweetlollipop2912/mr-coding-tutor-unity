f86015c007ac0569293cb50b30da562778ad1352
sangpham2710 <tansang.27.10.2003@gmail.com>
2025-03-10 19:44:24 +0700

    update

--- Assets/Scenes/CopyRotation.cs
+++ Assets/Scenes/CopyRotation.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CopyRotation : MonoBehaviour
 {
+    [SerializeField]
+    private Transform targetTransform;
+
+    [Tooltip("If true, will copy the world rotation. If false, will copy the local rotation.")]
+    [SerializeField]
+    private bool useWorldRotation = true;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (targetTransform != null)
+        {
+            if (useWorldRotation)
+            {
+                transform.rotation = targetTransform.rotation;
+            }
+            else
+            {
+                transform.localRotation = targetTransform.localRotation;
+            }
+        }
     }
 }
